b195a7e7eb7f280ae67b4f003159bc34c929b7f3
Ebenezer Baidoo <bebenezer149@gmail.com>
2026-07-10 13:39:13 +0000

    Another try deploy

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,14 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'create-product',
+        'login',
+        'register',
+        'products',
+        'sanctum/csrf-cookie'
+    ],
 
     'allowed_methods' => ['*'],
 
