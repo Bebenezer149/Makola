09f44add4fe8e4e777b739f30d468636528f42d1
Ebenezer Baidoo <bebenezer149@gmail.com>
2026-07-18 11:45:50 +0000

    New Fix for Orders

---
+++ database/migrations/2026_07_18_114210_update_order_status_constraint.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+         DB::statement("
+        ALTER TABLE orders
+        DROP CONSTRAINT orders_status_check
+    ");
+
+        DB::statement("
+        ALTER TABLE orders
+        ADD CONSTRAINT orders_status_check
+        CHECK (status IN ('PENDING','DELIVERED','CANCELLED','CONFIRMED'))
+    ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
