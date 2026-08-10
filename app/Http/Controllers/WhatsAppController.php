3fc0764d721dc78e2d38d97a43face11a70e857d
Ebenezer Baidoo <bebenezer149@gmail.com>
2026-08-10 23:10:10 +0000

    Whatsapp webhook

---
+++ app/Http/Controllers/WhatsAppController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class WhatsAppController extends Controller
+{
+    //
+    public function verify(Request $request){
+        $mode=$request->query('hub_mode');
+        $token=$request->query('hub_verify_token');
+        $challenge=$request->query('hub_challenge');
+
+        if($mode === "suscribe" && $token === config('services.whatsapp.whatsapp_verify_token')){
+            return response($challenge,200);
+        }
+
+        return response("Forbidden",403);
+    }
+
+    public function webhook(Request $request){
+         Log::info('WhatsApp Webhook Received', $request->all());
+
+        return response()->json([
+            'success' => true,
+        ], 200); 
+    }
+}
