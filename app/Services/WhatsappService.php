de4602845ed09850817285f41619d4dfea7bad3d
Ebenezer Baidoo <bebenezer149@gmail.com>
2026-08-11 12:09:11 +0000

    Whatsapp webhook

--- app/Services/WhatsappService.php
+++ app/Services/WhatsappService.php
@@ -10,9 +10,8 @@ class WhatsAppService
     public function sendMessage(string $recipientPhoneNumber, string $message)
     {
         $accessToken = config('services.whatsapp.access_token');
-        $verifyToken = config('services.whatsapp.verify_token');
         $phoneNumberId = config('services.whatsapp.phone_number_id');
-        return Http::withToken($accessToken)->acceptJson()->post("https://graph.facebook.com/v25.0/" . $phoneNumberId . '/' . $message, [
+        return Http::withToken($accessToken)->acceptJson()->post("https://graph.facebook.com/v25.0/{$phoneNumberId}/messages", [
             'messaging_product' => 'whatsapp',
             'to' => $recipientPhoneNumber,
             'type' => 'text',
